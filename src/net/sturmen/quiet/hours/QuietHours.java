d88ab40cbf6da00e25d4af391dbfa0943d00d9bc
Nick Tinsley <sturmen@outlook.com>
2012-08-18 12:38:49 -0400

    Revert "Settings Activity"
    This reverts commit bc7a4ec000794baee13fcbfefbdabe85e98cf52e.

--- src/net/sturmen/quiet/hours/QuietHours.java
+++ src/net/sturmen/quiet/hours/QuietHours.java
@@ -2,9 +2,7 @@ package net.sturmen.quiet.hours;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
 public class QuietHours extends Activity {
 
@@ -18,9 +16,5 @@ public class QuietHours extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_quiet_hours, menu);
         return true;
-    }
-    public void openSettings(View v){
-    	Intent intent = new Intent(this, Settings.class);
-    	startActivity(intent);
     }
 }
