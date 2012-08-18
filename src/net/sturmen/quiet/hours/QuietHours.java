bc7a4ec000794baee13fcbfefbdabe85e98cf52e
Nick Tinsley <sturmen@outlook.com>
2012-08-18 12:08:12 -0400

    Settings Activity
    It doesn't do much… yet.

--- src/net/sturmen/quiet/hours/QuietHours.java
+++ src/net/sturmen/quiet/hours/QuietHours.java
@@ -2,7 +2,9 @@ package net.sturmen.quiet.hours;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class QuietHours extends Activity {
 
@@ -16,5 +18,9 @@ public class QuietHours extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_quiet_hours, menu);
         return true;
+    }
+    public void openSettings(View v){
+    	Intent intent = new Intent(this, Settings.class);
+    	startActivity(intent);
     }
 }
