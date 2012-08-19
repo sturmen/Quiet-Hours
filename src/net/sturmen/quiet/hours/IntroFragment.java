8ea5db8eccac2edeea357942400f7ccde391ce68
Nick Tinsley <sturmen@outlook.com>
2012-08-18 21:45:36 -0400

    Major refactoring
    Moved a lot of stuff around and into fragments. Now much more "object
    oriented," if you know what I mean.

---
+++ src/net/sturmen/quiet/hours/IntroFragment.java
@@ -0,0 +1,22 @@
+package net.sturmen.quiet.hours;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IntroFragment extends Fragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.intro_quiet_hours, container, false);
+	}
+
+}
--- src/net/sturmen/quiet/hours/QuietHours.java
+++ src/net/sturmen/quiet/hours/QuietHours.java
@@ -2,19 +2,31 @@ package net.sturmen.quiet.hours;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.view.Menu;
+import android.view.View;
+
+
+
 
 public class QuietHours extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiet_hours);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_quiet_hours);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_quiet_hours, menu);
-        return true;
-    }
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_quiet_hours, menu);
+		return true;
+	}
+	public void openSettings(View view){
+		// Display the fragment as the main content.
+		FragmentTransaction transaction = getFragmentManager().beginTransaction().replace(R.id.intro, new SettingsFragment());
+		transaction.addToBackStack(null);
+		transaction.commit();
+	}
 }
