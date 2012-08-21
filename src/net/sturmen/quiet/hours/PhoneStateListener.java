1389541c8e612a099a4d1150be3b52c56db70d19
Nick Tinsley <sturmen@outlook.com>
2012-08-21 15:03:15 -0400

    Release Time!
    1.0 release!

--- src/net/sturmen/quiet/hours/PhoneStateListener.java
+++ src/net/sturmen/quiet/hours/PhoneStateListener.java
@@ -28,7 +28,7 @@ import android.preference.PreferenceManager;
 import android.provider.CalendarContract;
 import android.telephony.TelephonyManager;
 import android.text.format.Time;
-import android.util.Log;
+//import android.util.Log;
 import android.widget.Toast;
 
 public class PhoneStateListener extends BroadcastReceiver{ 
@@ -46,7 +46,7 @@ public class PhoneStateListener extends BroadcastReceiver{
 	//define the silent keyphrase as a CharSequence
 	private CharSequence silent = "_silent".subSequence(0,6);
 	//globally define the debug tag
-	private static final String tag = "QUIETHOURS";
+//	private static final String tag = "QUIETHOURS";
 	private static int original = 0;
 	private static boolean toastEnabled;
 
@@ -55,23 +55,23 @@ public class PhoneStateListener extends BroadcastReceiver{
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		if (prefs.getBoolean("pref_key_enabled", true)) {
 			toastEnabled = prefs.getBoolean("pref_key_toast", true);
-			Log.d(tag, "Toasts are " + toastEnabled);
+			//Log.d(tag, "Toasts are " + toastEnabled);
 			//initialize the audio manager
 			AudioManager ringer = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 			if (intent.getAction().equals("android.intent.action.PHONE_STATE"))
 			{
-				Log.d(tag, "Phone state changed.");
+				//Log.d(tag, "Phone state changed.");
 				//gather up the intent's extras into a bundle
 				Bundle extras = intent.getExtras();
 				//sanity check: if they exist...
 				if (extras != null) {
 					//get the state of the telephone
 					String state = extras.getString(TelephonyManager.EXTRA_STATE);
-					Log.d(tag, state);
+					//Log.d(tag, state);
 					if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)){ 
 						//store the previous phone state so we can return to it.
 						if (original == 0) original = ringer.getRingerMode();
-						Log.d(tag, "Original stored as " + state);
+						//Log.d(tag, "Original stored as " + state);
 						//and then this is where the magic happens...
 						getCurrent(context, ringer);
 					}
@@ -79,7 +79,7 @@ public class PhoneStateListener extends BroadcastReceiver{
 			}
 			else if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED"))
 			{
-				Log.d(tag, "SMS received.");
+				//Log.d(tag, "SMS received.");
 				if (original == 0) original = ringer.getRingerMode();
 				getCurrent(context, ringer);
 			}
@@ -93,20 +93,20 @@ public class PhoneStateListener extends BroadcastReceiver{
 		time.setToNow();
 		//convert the current time to milliseconds, ignoring Daylight Savings Time
 		long now = time.toMillis(true);
-		Log.d(tag, "" + now);
+		//Log.d(tag, "" + now);
 		//define what we want in a calendar event. One that starts before now AND ends after now.
 		String mSelectionClause = CalendarContract.Events.DTSTART + " <= " + now + " AND " + now + " <= " + CalendarContract.Events.DTEND;
-		Log.d(tag, "mSelectionClause =" +  mSelectionClause);
+		//Log.d(tag, "mSelectionClause =" +  mSelectionClause);
 		//we want the results sorted by which started first, just because
 		String mSortOrder = CalendarContract.Events.DTSTART + " ASC";
-		Log.d(tag, "mSortOrder =" + mSortOrder);
+		//Log.d(tag, "mSortOrder =" + mSortOrder);
 		//query the calendar provider and get the resulting cursor
 		Cursor mCursor = context.getContentResolver().query(CalendarContract.Events.CONTENT_URI, mProjection, mSelectionClause, null, mSortOrder);
 		//Move to first: must be done or everything dies
 		mCursor.moveToFirst();
 		if (mCursor.getCount() < 1) {
 			original = 0;
-			Log.d(tag, "No events. Putting on back.");
+			//Log.d(tag, "No events. Putting on back.");
 		} else {
 			//evaluate the first one
 			evaluate(context, ringer, mCursor);
@@ -125,7 +125,7 @@ public class PhoneStateListener extends BroadcastReceiver{
 		{
 			//if it's there, change to silent
 			ringer.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-			Log.d(tag, "Current event: " + title + ". Set to silent! (" + ringer.getRingerMode() + ")");
+			//Log.d(tag, "Current event: " + title + ". Set to silent! (" + ringer.getRingerMode() + ")");
 			//send notification to user
 			if (toastEnabled) Toast.makeText(context, "Current event: " + title + "\nSet to silent.", Toast.LENGTH_SHORT).show();
 		}
@@ -134,7 +134,7 @@ public class PhoneStateListener extends BroadcastReceiver{
 			//same, but do not override silent with vibrate!
 			if (ringer.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
 				ringer.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-				Log.d(tag, "Current event: " + title + ". Set to vibrate!(" + ringer.getRingerMode() + ")");
+				//Log.d(tag, "Current event: " + title + ". Set to vibrate!(" + ringer.getRingerMode() + ")");
 				//send notification to user
 				if (toastEnabled) Toast.makeText(context, "Current event: " + title + "\nSet to vibrate.", Toast.LENGTH_SHORT).show();
 			}
@@ -144,7 +144,7 @@ public class PhoneStateListener extends BroadcastReceiver{
 			if (original != 0) 
 			{
 				ringer.setRingerMode(original);
-				Log.d(tag, "Phone returned to " + original);
+				//Log.d(tag, "Phone returned to " + original);
 				original = 0;
 			}
 		}
